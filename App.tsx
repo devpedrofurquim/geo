73934426f6286f4b88c4bc26326d3225111b5ec9
devpedrofurquim <pedrofurquim.dev@gmail.com>
2024-08-23 10:05:51 -0300

    Watch Position Working

--- App.tsx
+++ App.tsx
@@ -1,27 +1,50 @@
-import { View, Text, PermissionsAndroid, Alert, Linking, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, PermissionsAndroid, Alert, TouchableOpacity, StyleSheet, Platform } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import Geolocation from '@react-native-community/geolocation';
 
-interface Location {
-  latitude: number;
-  longitude: number;
+interface Response {
+  coords: {
+    latitude: number,
+    longitude: number,
+    altitude: number | null,
+    accuracy: number,
+    altitudeAccuracy: number | null,
+    heading: number | null,
+    speed: number | null
+  },
+  timestamp: number
+}
+
+interface EResponse {
+  code: number;
+  message: string;
+  PERMISSION_DENIED: number;
+  POSITION_UNAVAILABLE: number;
+  TIMEOUT: number;
 }
 
 const App = () => {
-  const [currentLocation, setCurrentLocation] = useState<Location | null>(null);
+  const [currentLocation, setCurrentLocation] = useState<Response  | null>(null);
+  const [subscriptionId, setSubscriptionId] = useState<number | null>(null);
+  const [error, setError] = useState<EResponse | null>(null)
 
   useEffect(() => {
-     // Set configuration for Geolocation
     Geolocation.setRNConfiguration({
       skipPermissionRequests: false,
-      authorizationLevel: 'whenInUse', // iOS-only, optional
-      enableBackgroundLocationUpdates: true, // iOS-only, optional
-      locationProvider: 'auto', // Android-only, optional
+      authorizationLevel: 'always',
+      enableBackgroundLocationUpdates: true,
+      locationProvider: 'auto',
     });
 
     requestPermission().then(() => {
-      getCurrentLocation();
+      watchPosition();
     });
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      clearWatch();
+    };
   }, []);
 
   const requestPermission = async () => {
@@ -40,52 +63,84 @@ const App = () => {
       if (granted !== PermissionsAndroid.RESULTS.GRANTED) {
         Alert.alert('Permissão negada', 'Sem permissão para acessar localização.');
       }
+
+      const backgroundLocationGranted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_BACKGROUND_LOCATION,
+        {
+          title: 'Geo App Background Location',
+          message: 'Geo App quer acessar localização em segundo plano',
+          buttonNeutral: 'Pergunte Depois',
+          buttonNegative: 'Cancelar',
+          buttonPositive: 'Permitir',
+        }
+      )
+
+      if (backgroundLocationGranted !== PermissionsAndroid.RESULTS.GRANTED) {
+        Alert.alert('Permissão negada', 'Sem permissão para acessar localização em segundo plano.');
+      }
     } catch (err) {
       console.warn(err);
     }
   };
-
-  const getCurrentLocation = () => {
-    Geolocation.getCurrentPosition(
-      pos => {
-        const { latitude, longitude } = pos.coords;
-        setCurrentLocation({ latitude, longitude });
-      },
-      error => {
-        console.warn('Erro', error.message);
-        Alert.alert('Erro', 'Não foi possível obter a localização.');
-      }
-    );
-  };
-
-  const openMaps = async () => {
-    if (currentLocation) {
-      const { latitude, longitude } = currentLocation;
-      const url = `http://maps.google.com/maps?q=${latitude},${longitude}`;
   
-      console.log('Attempting to open URL:', url); // Log the URL for debugging
-  
-      try {
-        await Linking.openURL(url);
-      } catch (error) {
-        console.error('Erro ao abrir o URL:', error);
-        Alert.alert('Erro', 'Ocorreu um erro ao tentar abrir o URL.');
-      }
-    } else {
-      Alert.alert('Local não disponível', 'Não há localização atual para mostrar no mapa.');
+  const watchPosition = () => {
+    try {
+        const watchID = Geolocation.watchPosition(
+            (position) => {
+              console.log('watchPosition', position.coords.altitude, position.coords.latitude);
+              setCurrentLocation(position);
+            },
+            (error) => {
+              console.log('WatchPosition Error', JSON.stringify(error))
+              setError(error);
+            },
+            {
+              enableHighAccuracy: false,
+              timeout: 5000,
+              maximumAge: 0,
+              distanceFilter: 0,
+            }
+          );
+          setSubscriptionId(watchID);
+    } catch (e) {
+        console.warn('WatchPosition Error', e);
     }
+  }
+
+  const clearWatch = () => {
+    subscriptionId !== null && Geolocation.clearWatch(subscriptionId);
+    setSubscriptionId(null);
+    setCurrentLocation(null);
+    setError(null);
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Coordenadas</Text>
+      { currentLocation ? (
       <View style={styles.coordinatesContainer}>
-        <Text style={styles.coordinateText}>Latitude: {currentLocation ? currentLocation.latitude.toFixed(6) : 'carregando..'}</Text>
-        <Text style={styles.coordinateText}>Longitude: {currentLocation ? currentLocation.longitude.toFixed(6) : 'carregando..'}</Text>
+        <Text style={styles.coordinateText}>Latitude: {currentLocation.coords.latitude}</Text>
+        <Text style={styles.coordinateText}>Longitude: {currentLocation.coords.longitude}</Text>
       </View>
-      <TouchableOpacity style={styles.button} onPress={currentLocation ? openMaps : requestPermission}>
-        <Text style={styles.buttonText}>{currentLocation ? 'Abrir no Maps' : 'Obter Localização'}</Text>
-      </TouchableOpacity>
+      ) : (
+      <View style={styles.coordinatesContainer}>
+        <Text style={styles.coordinateText}>Carregando... {error?.code}</Text>
+      </View>
+      )
+      }
+      {
+        subscriptionId ? (
+            <TouchableOpacity style={styles.button} onPress={clearWatch}>
+                <Text>Limpar</Text>
+            </TouchableOpacity>
+        ) : (
+            <TouchableOpacity style={styles.button} onPress={watchPosition}>
+                <Text>Watch</Text>
+            </TouchableOpacity>
+        )
+      }
+      <View>
+    </View>
     </View>
   );
 };
@@ -110,6 +165,7 @@ const styles = StyleSheet.create({
   coordinateText: {
     fontSize: 18,
     color: '#666',
+    marginBottom: 4
   },
   button: {
     backgroundColor: '#007bff',
